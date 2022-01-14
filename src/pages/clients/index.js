be9c38197d1185629fc1c8a787fadf868af931ff
merin97 <merinmaryphilip@gmail.com>
2022-01-14 13:34:42 +0530

    changes

--- src/pages/clients/index.js
+++ src/pages/clients/index.js
@@ -3,20 +3,21 @@ import { useNavigate } from "react-router-dom";
 import Pagination from "../../components/pagination";
 
 export default function Clients() {
-  const itemCount = 100;
   const pageSize = 10;
 
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const [clients, setClients] = useState([]); // stateClients is declared
   const [currentPage, setCurrentPage] = useState(1);
   const [search, setSearch] = useState("");
+  const [itemCount, setItemCount] = useState(0);
 
   useEffect(() => {
     fetch("http://localhost:8000/clients") //api is fetched
       .then((res) => res.json())
       .then((response) => {
         setClients(response);
-      }); 
+        setItemCount(response.length);
+      });
   }, []);
 
   const onDelete = (id) => {
@@ -38,7 +39,9 @@ export default function Clients() {
   const getData = () => {
     const startIndex = currentPage * pageSize - pageSize;
     const endIndex = startIndex + pageSize;
-    console.log(clients .filter((x) => x.Name.toLowerCase().includes(search.toLowerCase())))
+    console.log(
+      clients.filter((x) => x.Name.toLowerCase().includes(search.toLowerCase()))
+    );
     return clients
       .filter((x) => x.Name.toLowerCase().includes(search.toLowerCase()))
       .slice(startIndex, endIndex);
@@ -54,15 +57,19 @@ export default function Clients() {
   return (
     <section className="my-3 container">
       <div className="d-flex justify-content-end mb-3">
-      <input
-        type="text"
-        placeholder="Search.."
-        name="search"
-        onChange={(e) => setSearch(e.target.value)}
-        autocomplete="off"
-      />
-      <button className=" mx-5 btn rounded-pill btn-primary" onClick={addClick}>ADD client</button>
-     
+        <input
+          type="text"
+          placeholder="Search.."
+          name="search"
+          onChange={(e) => setSearch(e.target.value)}
+          autocomplete="off"
+        />
+        <button
+          className=" mx-5 btn rounded-pill btn-primary"
+          onClick={addClick}
+        >
+          ADD client
+        </button>
       </div>
       <table className="table">
         <thead>
@@ -77,19 +84,19 @@ export default function Clients() {
           </tr>
         </thead>
         <tbody>
-          {getData().map((client) => (
-            <tr key ={client.id} className="table-primary">
-              <th scope="row">{client.id}</th>
-              <td>{client.Name}</td>
-              <td>{client.domain}</td>
-              <td>{client.manager_id}</td>
-              <td>{client.RevenuePerYear}</td>
+          {getData().map(({ id, Name, domain, manager_id, RevenuePerYear }) => (
+            <tr key={id} className="table-primary">
+              <th scope="row">{id}</th>
+              <td>{Name}</td>
+              <td>{domain}</td>
+              <td>{manager_id}</td>
+              <td>{RevenuePerYear}</td>
               <td>
-                <a onClick={() => onEdit(client.id)}>edit</a>
+                <a onClick={() => onEdit(id)}>edit</a>
               </td>
               <td>
                 <button
-                  onClick={() => onDelete(client.id)}
+                  onClick={() => onDelete(id)}
                   type="button"
                   class="btn-close"
                   aria-label="Close"
